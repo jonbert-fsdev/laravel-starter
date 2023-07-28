f8fda54de37865cfd80a91b4ad6bae37a2ae5f14
jonbert-fsdev <jonbertfsdev@gmail.com>
2023-07-28 11:34:17 +0800

    second try

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+// app/Http/Controllers/AuthController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/home');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        }
+
+        return redirect('/home');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        return redirect('/login');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel </title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
